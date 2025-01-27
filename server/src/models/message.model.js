ea3196400152557085264f31880c3705abc8a397
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-01-27 09:41:17 +0530

    Added Message Schema

---
+++ server/src/models/message.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema(
+    {
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            refPath: 'senderType',
+        },
+        senderType: {
+            type: String,
+            required: true,
+            enum: ['User', 'GoogleUser'],
+        },
+        receiverId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            refPath: 'receiverType',
+        },
+        receiverType: {
+            type: String,
+            required: true,
+            enum: ['User', 'GoogleUser'],
+        },
+        text: {
+            type: String,
+            default: '',
+        },
+        image: {
+            type: String,
+            default: '',
+        },
+          
+    },{timestamps: true}
+);
+
+export const Message = mongoose.model('Message', messageSchema);
+
+
